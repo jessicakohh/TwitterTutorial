7acf2ce76915fe3f3f26981807e28552a4b4df40
juyeong ko <juko95@me.com>
2023-01-15 18:23:59 +0900

    feat: Login UI

---
+++ TwitterTutorial/Controllers/Authentication/RegistrationController.swift
@@ -0,0 +1,28 @@
+//
+//  RegistrationControllerController.swift
+//  TwitterTutorial
+//
+//  Created by juyeong koh on 2023/01/15.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    // MARK: - Selectors
+
+    // MARK: - Helpers
+    func configureUI() {
+        view.backgroundColor = .twitterBlue
+    }
+
+}
+
