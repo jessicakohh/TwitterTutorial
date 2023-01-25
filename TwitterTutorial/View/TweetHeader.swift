4b1b95f63110b0e29e0a59b8d1fba1e014a3bcec
juyeong ko <juko95@me.com>
2023-01-25 18:02:02 +0900

    feat: Tweet Display Controller Setup

---
+++ TwitterTutorial/View/TweetHeader.swift
@@ -0,0 +1,25 @@
+//
+//  TweetHeader.swift
+//  TwitterTutorial
+//
+//  Created by juyeong koh on 2023/01/25.
+//
+
+import UIKit
+
+class TweetHeader: UICollectionReusableView {
+    // MARK: - Properties
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .systemPurple
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - LifeCycle
+    
+
+}
