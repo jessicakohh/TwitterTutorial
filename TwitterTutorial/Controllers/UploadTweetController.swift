88e3c2b76b0bf7134b7d2ba502a005fe53ac87d4
juyeong ko <juko95@me.com>
2023-01-29 16:08:55 +0900

    feat: Populating Data Using ViewModel

--- TwitterTutorial/Controllers/UploadTweetController.swift
+++ TwitterTutorial/Controllers/UploadTweetController.swift
@@ -41,6 +41,15 @@ class UploadTweetController: UIViewController {
         return iv
     }()
     
+    private let replyLabel: UILabel = {
+       let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textColor = .lightGray
+        label.text = "replying to @aa"
+//        label.widthAnchor.constraint(equalToConstant: view.frame.width).isActive = true
+        return label
+    }()
+    
     private let captionTextView = CaptionTextView()
     
     // MARK: - Lifecycle
@@ -92,21 +101,28 @@ class UploadTweetController: UIViewController {
         view.backgroundColor = .white
         configureNavigationBar()
         
-        let stack = UIStackView(arrangedSubviews: [profileImageView, captionTextView])
-        stack.axis = .horizontal
-        stack.spacing = 12
+        let imageCaptionStack = UIStackView(arrangedSubviews: [profileImageView, captionTextView])
+        imageCaptionStack.axis = .horizontal
+        imageCaptionStack.spacing = 12
         // 앞으로 나아갈 높이와 프로필 이미지의 캡션 TextView 동일하게 
-        stack.alignment = .leading
+        imageCaptionStack.alignment = .leading
+        
+        let stack = UIStackView(arrangedSubviews: [replyLabel, imageCaptionStack])
+        stack.axis = .vertical
+//        stack.alignment = .leading
+        stack.spacing = 12
         
         view.addSubview(stack)
         stack.anchor(top: view.safeAreaLayoutGuide.topAnchor,
-                     left: view.leftAnchor,
-                     right: view.rightAnchor,
-                     paddingTop: 16,
-                     paddingLeft: 16,
-                     paddingRight: 16)
+                     left: view.leftAnchor, right: view.rightAnchor,
+                     paddingTop: 16, paddingLeft: 16, paddingRight: 16)
         profileImageView.sd_setImage(with: user.profileImageUrl, completed: nil)
-        profileImageView.contentMode = .scaleAspectFill
+        actionButton.setTitle(viewModel.actionButtonTitle, for: .normal)
+        captionTextView.text = viewModel.placeholderText
+        
+        replyLabel.isHidden = !viewModel.shouldShowReplyLabel
+        guard let replyText = viewModel.replyText else { return }
+        replyLabel.text = replyText
     }
     
     func configureNavigationBar() {
