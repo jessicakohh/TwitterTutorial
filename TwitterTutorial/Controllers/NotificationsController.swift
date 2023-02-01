20c737ac9c628725af638fc3bbc2759111f96150
juyeong ko <juko95@me.com>
2023-02-01 16:13:47 +0900

    feat: Notification TableView Setup

--- TwitterTutorial/Controllers/NotificationsController.swift
+++ TwitterTutorial/Controllers/NotificationsController.swift
@@ -6,12 +6,15 @@
 //
 import UIKit
 
-class NotificationsController: UIViewController {
+private let reuseIdentifier = "NotificationCell"
+
+class NotificationsController: UITableViewController {
     
     // MARK: - Properties
     
-    // MARK: - LifeCycle
+    private var notifications = [Notification]()
     
+    // MARK: - LifeCycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +25,21 @@ class NotificationsController: UIViewController {
     func configureUI() {
         view.backgroundColor = .white
         navigationItem.title = "Notifications"
+        
+        tableView.register(NotificationCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.rowHeight = 60
+        tableView.separatorStyle = .none
+    }
+}
+
+
+extension NotificationsController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
     
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! NotificationCell
+        return cell
     }
 }
---
+++ TwitterTutorial/View/NotificationCell.swift
@@ -0,0 +1,60 @@
+//
+//  NotificationCell.swift
+//  TwitterTutorial
+//
+//  Created by juyeong koh on 2023/02/01.
+//
+
+import UIKit
+
+class NotificationCell: UITableViewCell {
+    
+    // MARK: - Properties
+    private lazy var profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.clipsToBounds = true
+        iv.setDimensions(width: 40, height: 40)
+        iv.layer.cornerRadius = 48 / 2
+        iv.backgroundColor = .twitterBlue
+        
+        // 탭 제스처 (UIImageView 버튼처럼 addTarget이 불가)
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleProfileImageTapped))
+        iv.addGestureRecognizer(tap)
+        iv.isUserInteractionEnabled = true
+        return iv
+    }()
+    
+    let notificationLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 2
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.text = "알림 테스트 문구"
+        return label
+    }()
+    
+    // MARK: - LifeCycle
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        let stack = UIStackView(arrangedSubviews: [profileImageView, notificationLabel])
+        stack.spacing = 8
+        stack.alignment = .center
+        
+        addSubview(stack)
+        stack.centerY(inView: self, leftAnchor: leftAnchor, paddingLeft: 12)
+        stack.anchor(right: rightAnchor, paddingRight: 12)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Selector
+    @objc func handleProfileImageTapped() {
+        
+    }
+
+
+}
